8fb15a4c82ca80e2e7b45088d1c226c52fbd3f36
codecason <kindom07@163.com>
2017-10-27 20:43:05 +0800

    add auto shell command

---
+++ tutorial/start.py
@@ -0,0 +1,8 @@
+# -coding:utf-8 -*-
+import os
+
+if __name__ == '__main__':
+    os.chdir('tutorial')
+    names = ['nowcoder', 'douban', 'shuimu']
+    for i in names:
+        os.system('scrapy crawl {}'.format(i))
