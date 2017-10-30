a9509b1f2fd3867b38d6a2cf796b5b02da975646
codecason <kindom07@163.com>
2017-10-30 16:12:19 +0800

    fix \n\n replace problem

--- tutorial/tutorial/utils/cleanTool.py
+++ tutorial/tutorial/utils/cleanTool.py
@@ -4,9 +4,9 @@ import os, scrapy, re
 class CleanTool():
     pattern1 = re.compile('<br><br>|<br>')
     # 必须删除script
-    pattern2 = re.compile('(<tr>|<td>|<script.*?>.*?</script>)\s+', re.S)
+    pattern2 = re.compile('(<tr>|<td>|<script.*?>.*?</script>)\s*', re.S)
     pattern3 = re.compile('(<div.*?>|<p>)\s*', re.S)
-    replaceSpan = re.compile('(<span>|</span>)\s+', re.S)
+    replaceSpan = re.compile('(<span>)|(</span>\s+)', re.S)
     pattern4 = re.compile('(</div>|</p>)\s*', re.S)
     pattern5 = re.compile('\n+')
 
@@ -15,7 +15,7 @@ class CleanTool():
         s = re.sub(self.pattern2, '', s)
         s = re.sub(self.pattern3, '\n', s)
         s = re.sub(self.pattern4, '\n\n', s)
-        s = re.sub(self.replaceSpan, '', s)
+        # s = re.sub(self.replaceSpan, '', s)
         s = re.sub(self.pattern5, '\n', s)
         return s.strip()
         # tag removed, strong, script removed, link reserved
