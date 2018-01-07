1d4ef899189bc6d2a85af29381926bc5dc6e5558
codecason <kindom07@163.com>
2018-01-07 13:51:37 +0800

    delete tutorail

--- rss/tutorial/spiders/douban_data/dirs.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import os
-DOUBAN_DATAFILE = os.path.dirname(__file__) + "/douban_post.jl"
-SHUIMU_DATADIR = os.path.join(os.path.dirname(__file__) ,os.path.pardir) + '/shuimu_data'
-# print DOUBAN_DATAFILE
